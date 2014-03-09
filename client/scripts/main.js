9063563c3aa4a7da33aeedbb6b745c36237eb17c
nfrasser <nfrasser@gmail.com>
2014-03-09 15:17:30 -0400

    Setup with es6 modules and requirejs seems to be working!

---
+++ client/scripts/main.js
@@ -0,0 +1,9 @@
+import App from "app";
+
+App.Router.map(function () {
+  this.route('courses');
+  this.route('todos');
+  this.route('settings');
+});
+
+export default App;
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  paths = {
+    scripts: 'client/scripts',
+    styles: 'client/styles',
+    templates: 'client/templates',
+    vendor: 'client/vendor',
+    build: 'client/build', // Change to the public folder
+    dist: 'client/dist' // Change to the public folder
+  };
+
+var transpiler = require('gulp-es6-module-transpiler'),
+  sass = require('gulp-sass'),
+  handlebars = require('gulp-ember-handlebars');
+
+gulp.task('scripts', function () {
+
+  // Transpile all JavaScripts into the build folder
+  return gulp.src(paths.scripts + '/**/*.js')
+    .pipe(transpiler({
+      type: 'amd'
+    }))
+    .pipe(gulp.dest(paths.build + '/'));
+
+});
 
 gulp.task('default', function () {
-	// place code for your default task here
+  // place code for your default task here
 });
